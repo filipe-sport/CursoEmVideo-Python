b31ff9a76720c889d1f0dce12ff0de7ff3baa3ad
Filipe <filipecesarufpe@gmail.com>
2024-03-17 10:34:55 -0300

    update

---
+++ Desafio2.py
@@ -0,0 +1,4 @@
+dia = int(input("Dia: "))
+mes = (input("Mês: "))
+ano = int(input("Ano: "))
+print(f"Você nasceu no dia: {dia} de {mes}  de {ano}")
---
+++ Desafio5.py
@@ -0,0 +1,19 @@
+#DESAFIO 5 - Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor
+num5 = int(input("Digite um número: "))
+print(f"Analisando o valor {num5}, seu antecessor é {num5-1} e o sucessor é {num5+1}")
+#DESAFIO 6 - Crie um algorithmo que leia um número e mostre o seu dobro, triplo e raiz quadrada#
+#num6 = int(input("Digite um número: "))
+#print(f"O dobro de {num6} vale {num6*2}.\n O triplo de {num6} vale {num6*3}.\n A raiz quadrada de {num6} vale {num6**2}")
+
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média#
+#Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milimetros#
+#Faça um programa que leia um número e mostrena tela a sua tabuada#
+#Crie um programa que leia quanto dinheiro tenho na carteira e mostre quantos dólares ela pode comprar#
+#Faça um programa que leia a largura e a altura de uma parede em metros. Calcule a sua área e a quantidade de tinta necessária para pinta-la,
+#sabendo que cada litro de tinta pinta uma área de 2m²"""
+#Faca um algoritimo que leia o preço de um produto e mostre seu novo preço com 5% de desconto.#
+#Faça um algorítimo que leia o salário de um funcionario e mostre seu novo salario com 15% de aumento#
+
+
+
+
