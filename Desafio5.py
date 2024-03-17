354ab0a16a7845d73fe4de66d3671a7c33cfdae3
Filipe <filipecesarufpe@gmail.com>
2024-03-17 11:25:17 -0300

    update

--- Desafio5.py
+++ Desafio5.py
@@ -1,12 +1,19 @@
 #DESAFIO 5 - Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor
 num5 = int(input("Digite um número: "))
 print(f"Analisando o valor {num5}, seu antecessor é {num5-1} e o sucessor é {num5+1}")
-#DESAFIO 6 - Crie um algorithmo que leia um número e mostre o seu dobro, triplo e raiz quadrada#
-#num6 = int(input("Digite um número: "))
-#print(f"O dobro de {num6} vale {num6*2}.\n O triplo de {num6} vale {num6*3}.\n A raiz quadrada de {num6} vale {num6**2}")
 
-#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média#
-#Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milimetros#
+#DESAFIO 6 - Crie um algorithmo que leia um número e mostre o seu dobro, triplo e raiz quadrada#
+num6 = int(input("Digite um número: "))
+print(f" O dobro de {num6} vale {num6*2}.\n O triplo de {num6} vale {num6*3}.\n A raiz quadrada de {num6} vale {num6**2}")
+
+#DESAFIO 7 -Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média#
+nota1 = float(input("Primeira nota do aluno: "))
+nota2 = float(input("Segunda nota do aluno: "))
+media = ((nota1+nota2)/2)
+print(f"A média entre {nota1} e {nota2} é igual a {media}")
+#DESAFIO 8 - Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milimetros#
+metros = float(input("Digite distancia em metros: "))
+print(f"A medida de {metros}m corresponde a:\n {metros/1000}km \n {metros/100}hm \n {metros/10} \n {metros*10:.0f}dm \n {metros*100:.0f}cm \n {metros*1000:.0f}mm ")
 #Faça um programa que leia um número e mostrena tela a sua tabuada#
 #Crie um programa que leia quanto dinheiro tenho na carteira e mostre quantos dólares ela pode comprar#
 #Faça um programa que leia a largura e a altura de uma parede em metros. Calcule a sua área e a quantidade de tinta necessária para pinta-la,
