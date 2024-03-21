c8840b769d2976f94c3c37b37005c58a55007dd9
Filipe <filipecesarufpe@gmail.com>
2024-03-21 19:13:06 -0300

    update

--- Desafio5.py
+++ Desafio5.py
@@ -11,9 +11,11 @@ nota1 = float(input("Primeira nota do aluno: "))
 nota2 = float(input("Segunda nota do aluno: "))
 media = ((nota1+nota2)/2)
 print(f"A média entre {nota1} e {nota2} é igual a {media}")
+
 #DESAFIO 8 - Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milimetros#
 metros = float(input("Digite distancia em metros: "))
-print(f"A medida de {metros}m corresponde a:\n {metros/1000}km \n {metros/100}hm \n {metros/10} \n {metros*10:.0f}dm \n {metros*100:.0f}cm \n {metros*1000:.0f}mm ")
+print(f"A medida de {metros}m corresponde a:\n {metros/1000}km \n {metros/100}hm \n {metros/10} \n"
+      f"{metros*10:.0f}dm \n {metros*100:.0f}cm \n {metros*1000:.0f}mm ")
 # DESAFIO 9 - Faça um programa que leia um número e mostrena tela a sua tabuada#
 num9 = int(input("Digite um número para ver sua tabuada: "))
 for i in range(10):
@@ -24,8 +26,13 @@ for i in range(10):
 dinheiro = float(input("Quanto dinheiro você tem na sua carteira? R$"))
 dolares = dinheiro * 5,16
 print(f"Com R${dinheiro} você pode comprar U${dolares:.1f}")
-#Faça um programa que leia a largura e a altura de uma parede em metros. Calcule a sua área e a quantidade de tinta necessária para pinta-la,cê 
-#sabendo que cada litro de tinta pinta uma área de 2m²"""
+# DESAFIO 11 - Faça um programa que leia a largura e a altura de uma parede em metros. Calcule a sua área e a quantidade de tinta necessária para pinta-la,
+#sabendo que cada litro de tinta pinta uma área de 2m²
+larg = float(input("Largura da parede: "))
+alt = float(input("Altura da parede: "))
+area = larg * alt
+print(f"Sua parede tem a dimensão de {larg} x {alt} e sua área é de {area:.2f}m².\n"
+      f"Para pintar essa parede, você precisará de {(area/2):.0f}L de tinta")
 #Faca um algoritimo que leia o preço de um produto e mostre seu novo preço com 5% de desconto.#
 #Faça um algorítimo que leia o salário de um funcionario e mostre seu novo salario com 15% de aumento#
 
