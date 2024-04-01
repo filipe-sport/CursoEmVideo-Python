a427c21d49b8ddd47313141e4916a7a67ab183a6
Filipe <filipecesarufpe@gmail.com>
2024-03-31 23:35:32 -0300

    upgrade

--- aulas/aula009.py
+++ aulas/aula009.py
@@ -2,7 +2,23 @@ frase = "Curso em Video Python"
 print(frase[9])
 print(frase[9:13])
 print(frase [9:21])
-print(frase [9:31:2])
+print(frase [9:21:2])
 print(frase[:5])
 print(frase[15:])
 print(frase [9::3])
+
+# Análise
+
+print(len(frase))
+print(frase.count("o",0,13))
+print(frase.find("deo"))
+print(frase.find("android"))
+print("Curso" in frase)
+
+# Transformação
+
+print(frase.replace("Python", "Android"))
+print(frase.upper())
+print(frase.lower())
+print(frase.capitalize())
+print(frase.title())
