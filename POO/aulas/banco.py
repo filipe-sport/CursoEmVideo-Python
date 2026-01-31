0aa496350ec22f164aa8956fec1e0d542c27492b
Filipe <filipecesar2019@gmail.com>
2026-01-31 20:20:21 -0300

    update

---
+++ POO/aulas/banco.py
@@ -0,0 +1,33 @@
+class ContaBancaria:
+    """
+    Docstring para ContaBancaria
+
+    Cria uma conta banária e permite fazer saques e depósitos
+    """
+
+    def __init__(self, id, nome, saldo = 0):
+        self.id = id
+        self.titular = nome
+        self.saldo = saldo
+        print(f'A conta {self.id} criada com sucesso. Saldo atual de R${self.saldo:,.2f}')
+
+    def __str__(self):
+        return f'A conta {self.id} de {self.titular} tem {self.saldo:,.2f} de saldo.'    
+    
+    def depositar(self, valor):
+        self.saldo += valor
+        print(f'Depósito de R${valor:,.2f} autorizado na conta {self.id}')
+
+    def sacar(self, valor):
+        if valor > self.saldo:{
+            print(f'Saque NEGADO de R${valor:,.2f} na conta {self.id}: SALDO INSUFICIENTE')
+        }
+        else:
+            self.saldo -= valor
+            print(f'Saque de R${valor:,.2f} autorizado na conta {self.id}')
+
+
+c1 = ContaBancaria(112, 'Gustavo', 3000)
+c1.depositar(500)
+c1.sacar(2000000)
+print(c1)
